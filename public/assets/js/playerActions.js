776905f6b878e313c9a02aea6f334cc3d8638986
srb2thepast <alexhazoume@gmail.com>
2022-12-09 18:03:50 +0000

    log

--- public/assets/js/playerActions.js
+++ public/assets/js/playerActions.js
@@ -171,7 +171,7 @@ define([], function () {
             touch = activetouches[0]
             playback.game.mouseX = (touch.pageX - gfx.xoffset) / gfx.width * 512;
             playback.game.mouseY = (touch.pageY - gfx.yoffset) / gfx.height * 384;
-            print(touch.pageX + " | " + touch.pageY);
+            console.log(touch.pageX + " | " + touch.pageY);
             movehistory.unshift({
                 x: playback.game.mouseX,
                 y: playback.game.mouseY,
