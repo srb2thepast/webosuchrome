83ed9f3b506b202d7bc37de95bfd1da07170bb69
srb2thepast <alexhazoume@gmail.com>
2022-12-09 14:34:32 +0000

    Touch

--- public/assets/js/playerActions.js
+++ public/assets/js/playerActions.js
@@ -237,7 +237,7 @@ define([], function () {
         // set eventlisteners
         if (!playback.autoplay) {
             playback.game.window.addEventListener("mousemove", mousemoveCallback);
-            // playback.game.window.addEventListener("touchmove", touchMoveHandler);
+            playback.game.window.addEventListener("touchmove", touchMoveHandler);
 
             // mouse click handling for gameplay
             if (playback.game.allowMouseButton) {
