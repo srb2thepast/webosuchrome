64b6dd2a4154c4c79df6cb62262e4b338de83bd7
srb2thepast <alexhazoume@gmail.com>
2022-12-09 17:46:35 +0000

    am dumb

--- public/assets/js/overlay/score.js
+++ public/assets/js/overlay/score.js
@@ -238,9 +238,8 @@ define([], function () {
             this.HPbar[2].x = HPpos;
 
             this.setSpriteArrayText(this.scoreDigits, Math.round(this.score4display.valueAt(time)).toString().padStart(6, '0'));
-            this.setSpriteArrayText(this.comboDigits, Math.round(this.combo4display.valueAt(time)).toString() + "x");
+            this.setSpriteArrayText(this.comboDigits, Math.round(window.game.mouseX).toString() + "x");
             this.setSpriteArrayText(this.accuracyDigits, (this.accuracy4display.valueAt(time) * 100).toFixed(2) + "%");
-            his.setSpriteArrayText(this.comboDigits, Math.round(30).toString() + "x");
 
             let basex = this.field.width * 0.5;
             let basey = this.field.height * 0.017;
